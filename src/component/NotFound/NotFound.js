98d6a47c9d1fd7779c9825ff5da2a77a18981398
Mousumi2001 <am6555264@gmail.com>
2022-10-11 18:56:36 +0600

    404 page create

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -3,11 +3,13 @@ import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div className='bg-gradient-to-r from-black-500 to-blue-500'>
-            <h1 className='text-9xl'>404</h1>
-            <h3>Sorry</h3>
-            <h5>This page has no found.</h5>
+        <div className='bg-gray-800 bgd text-white'>
+            <div className='text'>
+                <h1 className='text-9xl'>404</h1>
+                <h3 className='text-5xl'>Sorry</h3>
+                <h5 className='text-xl'>The page you're looking for was not found.</h5>
 
+            </div>
         </div>
     );
 };
